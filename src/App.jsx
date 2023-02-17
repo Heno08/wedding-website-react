bd844405718733069e6f2c565e8e14a5985f8212
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2023-02-17 13:48:44 +0100

    Added our story page and text

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import styles from './styles/app.module.css'
 import Header from './components/header';
 import Home from './components/home';
 import Rsvp from './components/rsvp';
+import OurStory from './components/our-story';
+import { useTranslation } from 'react-i18next';
 
 export default function App() {
+  const { t } = useTranslation();
   let display
   const [page, setPage] = useState('home');
 
   if(page === 'home') {
-    display = <Home />
-  } else if (page === 'rsvp'){
+    display = <Home t={t} />
+  } else if (page === 'rsvp') {
     display = <Rsvp />
+  } else if (page === 'ourstory') {
+    display = <OurStory t={t} />
   };
 
   return (
---
+++ src/components/our-story.jsx
@@ -0,0 +1,8 @@
+export default function OurStory( { t }) {
+  return (
+    <div>
+      <h2>{t("Our Story")}</h2>
+      <p>{t("Our story text")}</p>
+    </div>
+  )
+}
