470591300bd8738dc6471103ff3fee449312a1d7
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2023-03-19 17:35:04 +0100

    Reformatting + skeleton of Itinery page

---
+++ src/components/itinery.jsx
@@ -0,0 +1,29 @@
+export default function Itinery() {
+  return(
+    <div>
+      <h1>Itinery</h1>
+      <p>The theme of our wedding will be around 'Travel'. As such we would like to share a trip with you to mark our marriage:</p>
+      <h2>Ceremony</h2>
+        <p>Ceremony will take place at Castle Rushen. Please arrive for 2.30PM</p>
+        <p>The dress code will be smart casual</p>
+        <p>After the ceremony there will be time for some drink and some photos in the Castle Gardens</p>
+      <h2>Train Ride</h2>
+        <p>We will the aim to catch the 4.37 train from Castletown to Port Erin. We have resevered carriges on the train, but it will not wait for us!</p>
+        <p>There will be a bus to shuttle people from the Castle to the Station, but you are weclome to drive.</p>
+      <h2>Reception</h2>
+        <p>The start time for the recption 5.30pm at Foraging Vintners</p>
+        <p>We will start with some welcome drink (the iconic FV Fizz) and some Manx tapas (coutersy of Wild Thyme)</p>
+        <p>The planned start time for the speeches is:</p>
+        <p>Afterwards there will be a buffet (again thanks to Wild Thyme) before cake cutting and dancing!</p>
+        <p>The evening will be a free bar, excluding shots and cocktails, but you are more than welcome to buy these yourself</p>
+      <h2>Wedding Gift List</h2>
+        <p>Your presence at our wedding is more important to us than a gift, however if you would like to help contribute some money towards our honeymoon, this would really make our day!</p>
+        <p>Our honeymoon account is:</p>
+          <p>Name:</p>
+          <p>IBAN: IBAN4511</p>
+          <p>BIC: </p>
+        <p>There will also be the oportunity to make a donation at the reception.</p>
+    </div>
+  )
+}
+// justify text, arrage for heading then picture, picture then heading
