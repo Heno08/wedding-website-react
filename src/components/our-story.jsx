2093601b2036ad4b2db2c2a382c448753ce6d74a
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2023-03-27 10:25:43 +0200

    more pictures added and changed to CSS background

--- src/components/our-story.jsx
+++ src/components/our-story.jsx
@@ -1,7 +1,7 @@
 import styles from "../styles/our-story.module.css";
-import brugesTrip from "../images/Bruges-Trip.jpg";
-import lakeDistrict from "../images/Lake-D.jpeg";
-import vienna from "../images/Vienna-Palace.jpg";
+// import brugesTrip from "../images/Bruges-Trip.jpg";
+// import lakeDistrict from "../images/Lake-D.jpeg";
+// import vienna from "../images/Vienna-Palace.jpg";
 
 export default function OurStory( { t }) {
   return (
@@ -10,9 +10,12 @@ export default function OurStory( { t }) {
         <h2>{t("Our Story")}</h2>
         <p>{t("Our story text")}</p>
       </div>
-      <img alt="Bruges Trip" src={brugesTrip} className={[styles.pic1]}/>
+      <div className={[styles.pic1]}></div>
+      <div className={[styles.pic2]}></div>
+      <div className={[styles.pic3]}></div>
+      {/* <img alt="Bruges Trip" src={brugesTrip} className={[styles.pic1]}/>
       <img alt="Lake District" src={lakeDistrict} className={[styles.pic2]}/>
-      <img alt="Lake District" src={vienna} className={[styles.pic3]}/>
+      <img alt="Lake District" src={vienna} className={[styles.pic3]}/> */}
     </>
   )
 }
