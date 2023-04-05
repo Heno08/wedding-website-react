2b226cfd1b3164a40d2eee604f422c11dd785ac8
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2023-04-05 14:25:53 +0100

    some style changes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,12 @@
 import styles from "../styles/home.module.css";
-import handsphoto from "../images/H&SPhoto.jpg";
 
 export default function TextBox({ t }) {
   return (
-    <div className={styles.center}>
-      <h2 className={styles.textboxh2}>{t("Introduction")}</h2>
-      <h1 className={styles.textboxh1}>Henry and Sebastien</h1>
-      <h2 className={styles.textboxh2}>28 July 2023</h2>
-      <img alt="henry&seb" src={handsphoto}/>
-    </div>
+    <>
+      <h2 className={`${styles.textbox1} ${styles.center}`}>{t("Introduction")}</h2>
+      <h1 className={`${styles.textbox2} ${styles.center}`}>Henry and Sebastien</h1>
+      <h2 className={`${styles.textbox3} ${styles.center}`}>28 July 2023</h2>
+      <div className={`${styles.mainimage} ${styles.center}`}></div>
+    </>
   )
 }
