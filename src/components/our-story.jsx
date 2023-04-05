6324e8e09503a977def6cdb908e772bbca29e959
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2023-04-05 17:19:45 +0100

    added carousel to our-story page

--- src/components/our-story.jsx
+++ src/components/our-story.jsx
@@ -1,23 +1,33 @@
 import styles from "../styles/our-story.module.css";
-import Carousel from "./carousel";
 import pic1 from "../images/Bruges-Trip.jpg";
 import pic2 from "../images/Lake-D.jpeg";
 import pic3 from "../images/Vienna-Palace.jpg";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 export default function OurStory( { t }) {
-  const pictures = [pic1, pic2, pic3]
-
   return (
     <>
       <div className={styles.text}>
         <h2>{t("Our Story")}</h2>
         <p>{t("Our story text")}</p>
       </div>
-      {pictures.filter((picture, i )=> {
-        return (
-          <img src={picture} alt="" className={styles.pic1} key={i} />
-        )
-       })}
+      <div className={styles.carousel}>
+        <Carousel>
+          <div>
+            <img src={pic1} alt="" />
+            <p className="legend">Bruges 2020</p>
+          </div>
+          <div>
+            <img src={pic2} alt="" />
+            <p className="legend">Lake District 2021</p>
+          </div>
+          <div>
+            <img src={pic3} alt="" />
+            <p className="legend">Vienna 2021</p>
+          </div>
+        </Carousel>
+      </div>
       {/* <div className={[styles.pic1]}></div>
       <div className={[styles.pic2]}></div>
       <div className={[styles.pic3]}></div> */}
