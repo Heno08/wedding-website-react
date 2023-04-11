7c7cc1eb5af3891b240253360c54b1748c1d0b60
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2023-04-11 16:55:28 +0100

    added loading screen for rsvp

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,18 +7,18 @@ export default function Header({ setPage, setLang }) {
   }
   return (
     <>
-        <button className={`${styles.home} ${styles.headerbutton}`} onClick={() => setPage('home')}>Home</button>
-        <button className={`${styles.ourstory} ${styles.headerbutton}`} onClick={() => setPage('ourstory')}>Our Story</button>
-        <img src={Logo} alt="logo" className={styles.logo} />
-        <button className={`${styles.itinery} ${styles.headerbutton}`} onClick={() => setPage('itinery')}>Itinery</button>
-        <button className={`${styles.rsvp} ${styles.headerbutton}`} onClick={() => setPage('rsvp')}>RSVP</button>
-        <span className={`${styles.lang}`}>
-            <label htmlFor="lang">Language:</label>
-            <select name="lang" id="lang" onChange={handleChange}>
-              <option value="en">English</option>
-              <option value="fr">French</option>
-            </select>
-        </span>
+      <button className={`${styles.home} ${styles.headerbutton}`} onClick={() => setPage('home')}>Home</button>
+      <button className={`${styles.ourstory} ${styles.headerbutton}`} onClick={() => setPage('ourstory')}>Our Story</button>
+      <img src={Logo} alt="logo" className={styles.logo} />
+      <button className={`${styles.itinery} ${styles.headerbutton}`} onClick={() => setPage('itinery')}>Itinery</button>
+      <button className={`${styles.rsvp} ${styles.headerbutton}`} onClick={() => setPage('rsvp')}>RSVP</button>
+      <span className={`${styles.lang}`}>
+          <label htmlFor="lang">Language:</label>
+          <select name="lang" id="lang" onChange={handleChange}>
+            <option value="en">English</option>
+            <option value="fr">French</option>
+          </select>
+      </span>
     </>
   );
 }
